71ca83d52f7de2076e6e0b651f77f1c495842219
Andreea Schiau <schiau.andreea@gmail.com>
2017-07-20 09:47:38 +0300

    first commit -added the last one

---
+++ app/src/main/java/com/example/andreea/runaway/RunAway.java
@@ -0,0 +1,13 @@
+package com.example.andreea.runaway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RunAway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_away);
+    }
+}
